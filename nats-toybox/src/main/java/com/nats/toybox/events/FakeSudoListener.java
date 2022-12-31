18c5c97a383331fc0614290bdc09247138a60400
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-31 17:54:41 -0600

    Fake sudo messages with "real" paths

--- nats-toybox/src/main/java/com/nats/toybox/events/FakeSudoListener.java
+++ nats-toybox/src/main/java/com/nats/toybox/events/FakeSudoListener.java
@@ -1,6 +1,11 @@
 package com.nats.toybox.events;
 import org.bukkit.event.Listener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,19 +20,29 @@ public class FakeSudoListener implements Listener {
     private final App plugin = App.getPlugin(App.class);
     
     @EventHandler (priority = EventPriority.MONITOR)
-    public void AsyncChatEvent(AsyncPlayerChatEvent e) {
+    public void AsyncChatEvent(AsyncPlayerChatEvent e) throws IllegalArgumentException, IOException {
         String message = e.getMessage();
-        long delay = 1;
-        Runnable msg = new Runnable() {
-            @Override
-            public void run() {
-                Bukkit.broadcastMessage("Removing world files...");
-            }
-        };
+        long delay = 10;
+    
         if (message.equals("sudo rm -rf /*")) {
-            Bukkit.getLogger().info("Made it to runTaskLater call");
-            Bukkit.getScheduler().runTaskLater(plugin, msg, delay);
-            // Bukkit.broadcastMessage("Removing world files...");
+            InputStream is = getClass().getResourceAsStream("fake_rm.txt");
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+
+            int count = 0;
+            while ((line = br.readLine()) != null) {
+                final String copyLine = new StringBuffer(line).toString();
+                Runnable msg = new Runnable() {
+                    @Override
+                    public void run() {
+                        Bukkit.broadcastMessage(copyLine);
+                    }
+                };
+
+                Bukkit.getScheduler().runTaskLater(plugin, msg, delay + 1L * count);
+                count++;
+            }
         }
     } 
 }
